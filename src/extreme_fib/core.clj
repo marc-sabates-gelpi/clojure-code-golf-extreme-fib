a2f3dade9c0791356a457f5301cfc9d36d286be1
Marc Sabates Gelpi <marc.sabates.gelpi.workspace@gmail.com>
2017-07-26 19:23:26 +0100

    Fibonacci formula

--- src/extreme_fib/core.clj
+++ src/extreme_fib/core.clj
@@ -9,31 +9,23 @@
 ;; * Could pfib loop from k=end to k=0 so that it would call fact from 0 to n?
 ;; * Could pfib use concurrent loop?
 
-;; So far:
-;; [fib]
-;; extreme-fib.core> (extreme-fib 100 10000)
-;; "Elapsed time: 16.249762 msecs"
-;; (\3 \3 \6 \4 \4 \7 \6 \4 \8 \7 \6 \4 \3 \1 \7 \8 \3 \2 \6 \6 \6 \2 \1 \6 \1 \2 \0 \0 \5 \1 \0 \7 \5 \4 \3 \3 \1 \0 \3 \0 \2 \1 \4 \8 \4 \6 \0 \6 \8 \0 \0 \6 \3 \9 \0 \6 \5 \6 \4 \7 \6 \9 \9 \7 \4 \6 \8 \0 \0 \8 \1 \4 \4 \2 \1 \6 \6 \6 \6 \2 \3 \6 \8 \1 \5 \5 \5 \9 \5 \5 \1 \3 \6 \3 \3 \7 \3 \4 \0 \2)
-;; [pfib]
-;; extreme-fib.core> (extreme-fib 100 10000)
-;; "Elapsed time: 446490.359854 msecs"
-;; (\3 \3 \6 \4 \4 \7 \6 \4 \8 \7 \6 \4 \3 \1 \7 \8 \3 \2 \6 \6 \6 \2 \1 \6 \1 \2 \0 \0 \5 \1 \0 \7 \5 \4 \3 \3 \1 \0 \3 \0 \2 \1 \4 \8 \4 \6 \0 \6 \8 \0 \0 \6 \3 \9 \0 \6 \5 \6 \4 \7 \6 \9 \9 \7 \4 \6 \8 \0 \0 \8 \1 \4 \4 \2 \1 \6 \6 \6 \6 \2 \3 \6 \8 \1 \5 \5 \5 \9 \5 \5 \1 \3 \6 \3 \3 \7 \3 \4 \0 \2)
-
 (defn fib [n]
   (loop [a 0N b 1N c n]
     (if (>= 0 c)
       a
       (recur (+ a b) a (dec c)))))
 
+;; extreme-fib.core> (time (do (fact 100000) nil))
+;; "Elapsed time: 5530.880174 msecs"
+;; nil
 (def fact (memoize (fn [n]
                      (reduce * (range 1N (inc n))))))
 
-;; XXX All these implementations fail with:
-;; IndexOutOfBoundsException   clojure.lang.RT.nthFrom (RT.java:885)
+;; extreme-fib.core> (time (do (fact 100000) nil))
+;; OutOfMemoryError Java heap space  java.util.Arrays.copyOfRange (Arrays.java:3592)
 ;; (def facts (reductions * (iterate inc 1N)))
-;; (def facts (lazy-cat [1N] (map * facts (iterate inc 2N))))
 ;; (defn fact [n]
-;;   (nth facts (dec n)))
+;;   (nth facts (dec n) 1N))
 
 (def p (memoize (fn [n k]
                   (/ (fact n) (* (fact k) (fact (- n k)))))))
@@ -45,6 +37,22 @@
         sum
         (recur (dec k) (+ sum (p (- n k) k)))))))
 
+(defn afib [n]
+  (/
+   (-
+    (math/expt
+     (inc
+      (math/sqrt 5))
+     n)
+    (math/expt
+     (-
+      1
+      (math/sqrt 5))
+     n))
+    (*
+     (math/expt 2 n)
+     (math/sqrt 5))))
+
 (defn extreme-fib
   ([] (extreme-fib 1000 1000000000))
   ([digit n] (time (take digit
@@ -52,4 +60,6 @@
                           (fib n))))))
 
 (defn -main
-  [& args])
+  [& args]
+  (let [[a b & rest] args]
+  (println (extreme-fib (bigint a) (bigint b)))))
